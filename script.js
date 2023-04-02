b8b35865269d5e71a54a70babc472945348b7330
Gergő Gáspár <gaspar.gergo@gmail.com>
2023-04-02 23:07:02 +0200

    Write evaluateMatch function

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ function getComputerChoice() {
 			break;
 	}
 }
+
+function evaluateMatch (playerChoice, computerChoice) {
+	if (playerChoice === computerChoice) {
+		return `Tie! Both you and the computer chose ${playerChoice}!`;
+	}
+	else if (playerChoice === "rock") {
+		switch (computerChoice) {
+			case "scissors":
+					return `You won, ${playerChoice} beats ${computerChoice}!`;
+				break;
+			case "paper":
+					return `You lost, ${computerChoice} beats ${playerChoice}!`;
+				break;
+		}
+	}
+	else if (playerChoice === "paper") {
+		switch (computerChoice) {
+			case "rock":
+					return `You won, ${playerChoice} beats ${computerChoice}!`;
+				break;
+			case "scissors":
+					return `You lost, ${computerChoice} beats ${playerChoice}!`;
+				break;
+		}
+	}
+	else if (playerChoice === "scissors") {
+		switch (computerChoice) {
+			case "paper":
+					return `You won, ${playerChoice} beats ${computerChoice}!`;
+				break;
+			case "rock":
+					return `You lost, ${computerChoice} beats ${playerChoice}!`;
+				break;
+		}
+	}
+}
