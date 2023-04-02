a8a6bea4b0aee0f6bdddb576a33c1c56364723e3
Gergő Gáspár <gaspar.gergo@gmail.com>
2023-04-02 22:32:57 +0200

    Write getComputerChoice function and player prompt

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ let computerChoice = "";
 let playerChoice = "";
 let playerScore = 0;
 let computerScore = 0;
+
+playerChoice = prompt("Please choose: rock, paper or scissors?");
+
+function getComputerChoice() {
+	switch(Math.floor(Math.random()*3)) {
+		case 0:
+			return "rock";
+			break;
+		case 1:
+			return "paper";
+			break;
+		case 2:
+			return "scissors";
+			break;
+	}
+}
